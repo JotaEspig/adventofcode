134f793ecb2856ed875748595db330981aa7364d
João Vitor Espig <jotaespig@gmail.com>
2023-12-01 11:45:41 -0300

    add solution for the first problem

--- 1/solution.cpp
+++ 1/solution.cpp
@@ -1,13 +1,43 @@
+#include <cctype>
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+int get_value_in_line(string buff)
+{
+    string value_str = "";
+    // find first
+    for (auto c : buff)
+    {
+        if (isdigit(c))
+        {
+            value_str += c;
+            break;
+        }
+    }
+    for (auto it = buff.end() - 1; it >= buff.begin(); --it)
+    {
+        if (isdigit(*it))
+        {
+            value_str += *it;
+            break;
+        }
+    }
+
+    return stoi(value_str);
+}
 
 int main()
 {
     long sum = 0;
-    long a;
-    while (cin >> a)
+    string buff;
+
+    while (getline(cin, buff))
     {
+        int a = get_value_in_line(buff);
+        cout << buff << endl
+             << a << endl;
         sum += a;
     }
     cout << sum << endl;
