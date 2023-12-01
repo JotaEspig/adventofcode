867220131d02ef1bcda7d1e0e9db8b3b03d0bebf
João Vitor Espig <jotaespig@gmail.com>
2023-12-01 11:25:42 -0300

    initial commit

---
+++ 1/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long sum = 0;
+    long a;
+    while (cin >> a)
+    {
+        sum += a;
+    }
+    cout << sum << endl;
+    return 0;
+}
