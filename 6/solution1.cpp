3f7984ec268e95f4bb42c9dbc12dcd8c772a9e1f
João Vitor Espig <jotaespig@gmail.com>
2023-12-07 00:32:42 -0300

    finished day 6

---
+++ 6/solution1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+#define size 4
+
+int main()
+{
+    int times[size] = {48, 98, 90, 83};
+    int dist[size] = {390, 1103, 1112, 1360};
+    int product = 1;
+    for (int i = 0; i < size; ++i)
+    {
+        int t = times[i];
+        int s = dist[i];
+        double delta = pow(t, 2) - 4 * -1 * -s;
+        double x1 = (-t - sqrt(delta)) / -2;
+        double x2 = (-t + sqrt(delta)) / -2;
+
+        int start = ceil(min(x1, x2));
+        int end = floor(max(x1, x2));
+        if (start == min(x1, x2))
+            ++start;
+        if (end == max(x1, x2))
+            --end;
+        cout << start << " " << end << endl;
+        product *= end - start + 1; 
+    }
+
+    cout << product << endl;
+    return 0;
+}
---
+++ 6/solution2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long t = 48989083;
+    long s = 390110311121360;
+    double delta = pow(t, 2) - 4 * -1 * -s;
+    double x1 = (-t - sqrt(delta)) / -2;
+    double x2 = (-t + sqrt(delta)) / -2;
+
+    long start = ceil(min(x1, x2));
+    long end = floor(max(x1, x2));
+    if (start == min(x1, x2))
+        ++start;
+    if (end == max(x1, x2))
+        --end;
+
+    cout << start << " " << end << endl;
+
+    long product = end - start + 1; 
+
+    cout << product << endl;
+    return 0;
+}
